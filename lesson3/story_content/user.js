1c64986416261fb83c2be0d27fcd59fe42b3174a
Mibza <72165318+Mibza@users.noreply.github.com>
2021-03-11 23:03:42 +0100

    Add files via upload

--- lesson3/story_content/user.js
+++ lesson3/story_content/user.js
@@ -1,1 +1,15 @@
-// User defined code goes here
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ru1L47aPlO":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var endCourseMotherfucker = true
+}
+
