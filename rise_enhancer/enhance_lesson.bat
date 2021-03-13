12079dd6a8f795e02c19d7345a7065a80c67920d
ray <you@example.com>
2021-03-13 08:46:29 +0100

    copy folder wokring

---
+++ rise_enhancer/enhance_lesson.bat
@@ -0,0 +1,45 @@
+@echo off
+Title Browse4Folder
+Color 0A
+Call :Browse4Folder "Choose source lesson to enhance" 
+echo Enhancing Project "%Location%"
+Call :CopyFiles 
+pause & exit
+
+
+
+
+
+
+
+
+
+:CopyFiles
+xcopy ".\dist" "%Location%\dist\" /E
+xcopy ".\jquery-2.1.1.min.js" "%Location%\" /E
+xcopy ".\zsolt.js" "%Location%\" /E
+goto :eof
+
+::***************************************************************************
+:Browse4Folder
+set Location=
+set vbs="%temp%\_.vbs"
+set cmd="%temp%\_.cmd"
+for %%f in (%vbs% %cmd%) do if exist %%f del %%f
+for %%g in ("vbs cmd") do if defined %%g set %%g=
+(
+    echo set shell=WScript.CreateObject("Shell.Application"^) 
+    echo set f=shell.BrowseForFolder(0,"%~1",0,"%~2"^) 
+    echo if typename(f^)="Nothing" Then  
+    echo wscript.echo "set Location=Dialog Cancelled" 
+    echo WScript.Quit(1^)
+    echo end if 
+    echo set fs=f.Items(^):set fi=fs.Item(^) 
+    echo p=fi.Path:wscript.echo "set Location=" ^& p
+)>%vbs%
+cscript //nologo %vbs% > %cmd%
+for /f "delims=" %%a in (%cmd%) do %%a
+for %%f in (%vbs% %cmd%) do if exist %%f del /f /q %%f
+for %%g in ("vbs cmd") do if defined %%g set %%g=
+goto :eof
+::*****
