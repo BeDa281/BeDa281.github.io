b284d5a44cd65c5af5e36835c79b563d86f77705
Mibza <72165318+Mibza@users.noreply.github.com>
2021-04-10 09:25:29 +0200

    Add files via upload

---
+++ lesson3/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://nsGm0N6r9uVuYwMtCxUfTdlRpZrx4ooP_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Beispielkurs"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Beispielkur mit allen Elementen, die generell verwendet werden k&amp;ouml;nnen.&lt;/p&gt;"
+};
