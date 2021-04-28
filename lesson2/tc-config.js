52aa806a545046bffc3c180a92fc51774fc66246
Mibza <72165318+Mibza@users.noreply.github.com>
2021-04-28 15:56:50 +0200

    Add files via upload

---
+++ lesson2/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://sBHy22Hwnara2szwYoBNAu28jPOfoekN_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Quiz"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;In diesem Bereich finden Sie ein Quiz mit 10 kurze Fragen aus allen Pr&amp;uuml;fungsbereichen. Sie m&amp;uuml;ssen 80% der Fragen richtig beantworten, um das Quiz abzuschlie&amp;szlig;en.&lt;/p&gt;"
+};
