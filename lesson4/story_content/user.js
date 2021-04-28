cc59948a1627edd13c6b6c0110de5a1fc27f3f1a
Mibza <72165318+Mibza@users.noreply.github.com>
2021-04-28 08:16:24 +0200

    Add files via upload

---
+++ lesson4/story_content/user.js
@@ -0,0 +1,15 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5vh5ltGJq4z":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var endCourseMotherfucker = true
+}
+
