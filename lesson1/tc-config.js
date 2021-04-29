a1a60a4f2d7245f70430c6e8cb0d24e0ac0426db
Mibza <72165318+Mibza@users.noreply.github.com>
2021-04-29 19:43:19 +0200

    Add files via upload

---
+++ lesson1/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://rWkLBHTOTr2zwkJ7xgjuB3lgstuZ66NH_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Handlungsfeld 1: Technik und Gestaltung"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;In diesem Bereich finden Sie 10 Fragen und L&amp;ouml;sungen zum Handlungsfeld 1: Technik und Gestaltung. Die Fragen k&amp;ouml;nnen mehrere Abschnitt beinhalten (A - F), die nacheinander bearbeitet werden sollten.&amp;nbsp;&lt;/p&gt;&lt;p&gt;Viel Erfolg!&lt;/p&gt;"
+};
