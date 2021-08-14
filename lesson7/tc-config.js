b4fe33ba211eb455a90d9640641736653bb718f6
Mibza <72165318+Mibza@users.noreply.github.com>
2021-08-14 20:48:15 +0200

    Add files via upload

---
+++ lesson7/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://ObJcoIKjCt4bndWglcic18s6K-f2ru5h_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Quiz 2"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Mach dieses Quiz&lt;/p&gt;"
+};
