98388675ce3daaf8c65a9e5e407f50458ce37ba5
Mibza <72165318+Mibza@users.noreply.github.com>
2021-08-14 20:50:58 +0200

    Add files via upload

--- lesson6/tc-config.js
+++ lesson6/tc-config.js
@@ -9,12 +9,12 @@ TC_COURSE_ID = "http://76LtE20XJ3Ze9hL9jnl_u6cnrTPwdjTM_rise"
 // CourseName for the activity
 //
 TC_COURSE_NAME = {
-  "en-US": "Testing Text Input"
+  "en-US": "Testing"
 };
 
 //
 // CourseDesc for the activity
 //
 TC_COURSE_DESC = {
-  "en-US": "&lt;p&gt;Testen von text input Bl&amp;ouml;cken, embedded &amp;uuml;ber Storyline&lt;/p&gt;"
+  "en-US": "&lt;ul&gt;&lt;li&gt;Text Input Bl&amp;ouml;cke, embedded &amp;uuml;ber Storyline&lt;/li&gt;&lt;li&gt;Pop-up L&amp;ouml;sungen&lt;/li&gt;&lt;/ul&gt;"
 };
