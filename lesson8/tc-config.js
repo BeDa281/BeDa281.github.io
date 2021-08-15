8d807d8a2c9a86a6450183a733d12e85580e8a89
Mibza <72165318+Mibza@users.noreply.github.com>
2021-08-15 18:27:55 +0200

    Add files via upload

---
+++ lesson8/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://HXm1MGUEkK8gSQgWNLfo7sQZhlFBeN2m_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Quiz - Test xAPI Settings"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Quiz mit xAPI Settings &amp;quot;track using quiz result&amp;quot;&lt;/p&gt;"
+};
