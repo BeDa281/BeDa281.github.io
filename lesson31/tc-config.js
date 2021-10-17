808c2638eec6d79f9f392fd1876f4bdfdee22968
Mona Lisa <Mona.Lisa@example.com>
2021-10-17 12:01:47 +0200

    Fragen Mitarbeiterabrechnung

---
+++ lesson31/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://sYgqLpN1a6pV4JF4D0n9tjvtiG4rx47a_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Mitarbeiterabrechnung - Teil 1"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;In diesem Bereich finden Sie ein Quiz mit einigen Fragen. Sie m&amp;uuml;ssen 100% der Fragen richtig beantworten, um das Quiz abzuschlie&amp;szlig;en.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Sie k&amp;ouml;nnen das Quiz beliebig oft wiederholen.&lt;/p&gt;"
+};
