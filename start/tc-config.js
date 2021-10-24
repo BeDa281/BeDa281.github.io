1dac59f3b2220c4b0abfa2dd1a45570ad06a1fec
Mona Lisa <Mona.Lisa@example.com>
2021-10-24 12:16:40 +0200

    added start course

---
+++ start/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://1qI4yd5DkF4H04QHNmzQ7ZwHN654wQe9_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Einführung"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;In diesem Kurs erh&amp;auml;ltst du eine kurze Einf&amp;uuml;hrung in die Nutzung der App.&lt;/p&gt;"
+};
