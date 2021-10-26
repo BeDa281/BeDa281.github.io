25cd9c3a9d7eee474bf3b0f02bac618115238f5e
Mona Lisa <Mona.Lisa@example.com>
2021-10-26 19:19:26 +0200

    Maschienenkostenrechnung

---
+++ lesson40/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://d70M8drTPm-rmHauJAJxCwz8L_s7qe-V_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Maschinenkostenrechnung - Grundlagen"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
