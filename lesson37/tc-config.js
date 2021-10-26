d5769db779e80a5c2462aabd11c8ab00ec6cfcb5
Mona Lisa <Mona.Lisa@example.com>
2021-10-26 16:20:33 +0200

    Kooperationen - 2 courses

---
+++ lesson37/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://HfL1ePJ2ySQ0mZa4R_fA2e70rLEqVsUN_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Kooperationen - Grundlagen"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;"
+};
