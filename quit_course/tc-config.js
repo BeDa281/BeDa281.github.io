046fec768bda4fca5c6a60a3960ee558ffba8a82
Your Name <you@example.com>
2021-10-27 16:56:41 +0200

    quit_coures_test

---
+++ quit_course/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://UIP73bJ0yfkFelT8_V0_JZNmIM5HBjFk_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Kurs beenden"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;In diesem Kurs erh&amp;auml;ltst du eine kurze Einf&amp;uuml;hrung in die Nutzung der App.&lt;/p&gt;"
+};
