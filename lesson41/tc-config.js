b8c7e28f7dc4091e23fa0f5d40090720e591afee
MonaLisa <mona.lisae@inovatik.de>
2021-12-17 11:20:23 +0100

    corrections vob 3

---
+++ lesson41/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://Oxl5ATCbHY-AgG-SZgvFTiGqsSel4IJM_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Zinsberechnung - Grundlagen"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
