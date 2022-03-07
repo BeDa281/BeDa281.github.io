418adab9aae288d80140c304cccec53348d62eb2
MonaLisa <mona.lisae@inovatik.de>
2022-03-07 12:08:29 +0100

    new lessons Teil4

---
+++ lesson106/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://vydpeI-JuxPDIuvN7EPn1K8FSo7zew57_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Ausbildung vorbereiten - Quiz 2"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;In diesem Bereich finden Sie ein Quiz mit einigen Fragen. Sie m&amp;uuml;ssen 100% der Fragen richtig beantworten, um das Quiz abzuschlie&amp;szlig;en.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Sie k&amp;ouml;nnen das Quiz beliebig oft wiederholen.&lt;/p&gt;"
+};
