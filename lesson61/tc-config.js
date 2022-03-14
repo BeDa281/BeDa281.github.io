152112ebda106d8246c7157b450762cca0426cf8
MonaLisa <mona.lisae@inovatik.de>
2022-03-14 11:46:46 +0100

    new courses - BAKO

---
+++ lesson61/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://3qXuVID20RXNQ3Cz70V1IWLI3EEpT2do_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Rissinjektion - Quiz 1"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;In diesem Bereich finden Sie ein Quiz mit einigen Fragen. Sie m&amp;uuml;ssen 100% der Fragen richtig beantworten, um das Quiz abzuschlie&amp;szlig;en.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Sie k&amp;ouml;nnen das Quiz beliebig oft wiederholen.&lt;/p&gt;"
+};
