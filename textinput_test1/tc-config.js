b7fc6ccaeaac4db7efce3eca5b112ee686f2aa7b
MonaLisa <mona.lisae@inovatik.de>
2022-03-30 16:04:37 +0200

    test lessons

---
+++ textinput_test1/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://LOcdA-DNoIiC9QQ71JmfSJCF6tKYi55E_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "textinput_test1"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
