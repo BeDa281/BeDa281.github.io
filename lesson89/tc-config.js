b89739ccaf3dafa79dfc8a11e62e3a39e9c9e132
MonaLisa <mona.lisae@inovatik.de>
2022-06-22 10:52:41 +0200

    4 new lessons

---
+++ lesson89/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://mXrMmVbh05s9Fd0-feeH1KB6cUH1eCbX_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Arbeitssicherheit - Gerüste"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
---
+++ lesson90/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://rQnuxkRLHsdS-I3HS-vIh-l21_zgApy2_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Betriebsführung - Formelsammlung"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
