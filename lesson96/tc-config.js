23c77f96b83ff25f72e3e493f7b54f6a3167cf84
MonaLisa <mona.lisae@inovatik.de>
2023-01-09 21:32:39 +0100

    Schneelast

---
+++ lesson96/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://-v1_6eQbVPjA3SmMFpD3XRelfbEuqROu_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Statik - Schneelast"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
