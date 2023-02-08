abfc7ce848ceebdb2de07f3bd20f4d33c72b55ac
MonaLisa <mona.lisae@inovatik.de>
2023-02-08 20:57:35 +0100

    Flachdach 1 & 2

---
+++ lesson127/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://RuIiUtckDrxFKYdMjVMo4WT0iHDnrby__rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Flachdach - Quiz 1"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;In diesem Bereich finden Sie ein Quiz mit einigen Fragen. Sie m&amp;uuml;ssen 100% der Fragen richtig beantworten, um das Quiz abzuschlie&amp;szlig;en.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Sie k&amp;ouml;nnen das Quiz beliebig oft wiederholen.&lt;/p&gt;"
+};
