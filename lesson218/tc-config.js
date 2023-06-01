01afd1b95c3333283af87e0161c019c20b3b1635
MonaLisa <mona.lisae@inovatik.de>
2023-06-01 09:19:53 +0200

    Treppenbau

---
+++ lesson218/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://7YgC5nJIK1O_2zQnEuatMWxwzm44sUXu_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Treppenbau"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
