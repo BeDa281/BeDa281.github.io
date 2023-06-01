ba2fd39a8f9c4a85175831899795f9fef0f924c0
MonaLisa <mona.lisae@inovatik.de>
2023-06-01 12:08:55 +0200

    update on lesson 42

--- lesson42/tc-config.js
+++ lesson42/tc-config.js
@@ -3,18 +3,18 @@
 //
 // ActivityID that is sent for the statement's object
 //
-TC_COURSE_ID = "http://Oxl5ATCbHY-AgG-SZgvFTiGqsSel4IJM_rise"
+TC_COURSE_ID = "http://D6_ZTixkZWkk1CaCMXWyxOGCCb0WThea_rise"
 
 //
 // CourseName for the activity
 //
 TC_COURSE_NAME = {
-  "en-US": "Zinsberechnung - Grundlagen"
+  "en-US": "Zinsberechnung"
 };
 
 //
 // CourseDesc for the activity
 //
 TC_COURSE_DESC = {
-  "en-US": ""
+  "en-US": "&lt;p&gt;In diesem Bereich finden Sie ein Quiz mit einigen Fragen. Sie m&amp;uuml;ssen 100% der Fragen richtig beantworten, um das Quiz abzuschlie&amp;szlig;en.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Sie k&amp;ouml;nnen das Quiz beliebig oft wiederholen.&lt;/p&gt;"
 };
