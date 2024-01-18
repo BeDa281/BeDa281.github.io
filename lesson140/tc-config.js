bf8264018d640e55b6ddbea49063d30ca563d368
MonaLisa <mona.lisae@inovatik.de>
2024-01-18 09:59:59 +0100

    Kreisbogenmethode

---
+++ lesson140/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://YdMPRVlPXBqBZAMPorm6UJbg94CQth8Z_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Kreisbogenmethode"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
