1d998868f45efda9fea218f32e75f61201bc5e61
MonaLisa <mona.lisae@inovatik.de>
2024-12-13 11:48:24 +0100

    New lesson

---
+++ lesson152/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://6lviM9s-PRuI0uoZqsgQrKqSVGdaqyPC_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Bestimmung statischer System"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
