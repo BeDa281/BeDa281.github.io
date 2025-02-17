83413e618213ebd4303cdf35fe6bd4af6758993b
MonaLisa <mona.lisae@inovatik.de>
2025-02-17 16:29:20 +0100

    Prüfungsvorbereitung

---
+++ lesson229/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://5vFeDXRo4qkOOBKvO93RRIcfdRmuW-Rn_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "AEVO - Prüfungsvorbereitung"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;div data-editor-id=&#34;f2147b9b-15d2-43a2-b196-45542a450c2b&#34;&gt;&lt;p&gt;In diesem Quiz müssen Sie 20 zufällige Fragen zu den Themen der AEVO Prüfung beantworten. Bei jedem Start werden die Fragen neu zusammengestellt. &lt;/p&gt;&lt;p&gt;&lt;/p&gt;&lt;p&gt;Viel Erfolg!&lt;/p&gt;&lt;/div&gt;"
+};
