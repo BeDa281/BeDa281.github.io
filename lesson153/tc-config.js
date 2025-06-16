e84396bf8141d0123fee4f2dbd1cde5a97bdd37e
MonaLisa <mona.lisae@inovatik.de>
2025-06-16 08:01:42 +0200

    Teil 3 - 1

---
+++ lesson153/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://UzscLG_qsePjhm9Ry5hOevBLktPIPeFV_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "HF I - Wettbewerbsfähigkeit von Unternehmen beurteilen"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;div data-editor-id=&#34;ca833611-250a-4cc8-aa7b-884fd5325075&#34;&gt;&lt;p&gt;In diesem Quiz müssen Sie 10 zufällige Fragen zu den Themen des HF I - Wettbewerbsfähigkeit von Unternehmen beurteilen beantworten. Bei jedem Start werden die Fragen neu zusammengestellt.&lt;/p&gt;&lt;p&gt;&lt;/p&gt;&lt;p&gt;Viel Erfolg!&lt;/p&gt;&lt;/div&gt;"
+};
