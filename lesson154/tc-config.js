96c92ef02a25b53810db21a4f880a44567f2cafd
MonaLisa <mona.lisae@inovatik.de>
2025-08-11 13:15:48 +0200

    154 - update

---
+++ lesson154/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://_YEtoXXY_WphBJuQ9YW8k4EYKiE_Nw8f_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "HF II - Gründungs- und Übernahmeaktivitäten vorbereiten, durchführen und bewerten"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;div data-editor-id=&#34;a8d71fb6-e76d-43f7-8cbf-f683913cd575&#34;&gt;&lt;p&gt;In diesem Quiz müssen Sie 10 zufällige Fragen zu den Themen des HF II - Gründungs- und Übernahmeaktivitäten vorbereiten, durchführen und bewerten&amp;nbsp;beantworten. Bei jedem Start werden die Fragen neu zusammengestellt.&lt;/p&gt;&lt;p&gt;&lt;/p&gt;&lt;p&gt;Viel Erfolg!&lt;/p&gt;&lt;/div&gt;"
+};
