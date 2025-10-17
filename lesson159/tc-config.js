e828af1ed917456f67113a027a63ed8e86c9ee9f
MonaLisa <mona.lisae@inovatik.de>
2025-10-17 10:26:45 +0200

    6 new lessons

---
+++ lesson159/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://rEsq5aheqxlZpg_nIvZvEq0vG-RAw4ad_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Wettbewerbsfähigkeit von Unternehmen beurteilen - Quiz 5"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;div data-editor-id=&#34;7b9d4ead-201f-456b-ab9a-0bd911a5a2f2&#34;&gt;&lt;p&gt;In diesem Quiz müssen Sie 10 Fragen zu den Themen des HF I - Wettbewerbsfähigkeit von Unternehmen beurteilen beantworten. &lt;/p&gt;&lt;p&gt;&lt;/p&gt;&lt;p&gt;Viel Erfolg!&lt;/p&gt;&lt;/div&gt;"
+};
